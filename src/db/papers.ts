baaf5f95e41a727aebef0699ee3103b2fdc2c3b6
NishantGupta2325 <nishant.gupta@talentrecruit.com>
2024-06-16 00:58:46 +0530

    build fix

--- src/db/papers.ts
+++ src/db/papers.ts
@@ -1,37 +1,22 @@
-import mongoose, { Document, Schema, Model } from 'mongoose';
+import mongoose, { Schema, Document, Model } from "mongoose";
 
 interface IPaper extends Document {
   file: Buffer;
   subject: string;
   slot: string;
   year: string;
-  exam: 'cat1' | 'cat2' | 'fat';
+  exam: "cat1" | "cat2" | "fat";
 }
 
-const PaperSchema: Schema<IPaper> = new Schema({
-  file: {
-    type: Buffer,
-    required: true,
-  },
-  subject: {
-    type: String,
-    required: true,
-  },
-  slot: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: String,
-    required: true,
-  },
-  exam: {
-    type: String,
-    enum: ['cat1', 'cat2', 'fat'],
-    required: true,
-  },
+const paperSchema = new Schema<IPaper>({
+  file: { type: Buffer, required: true },
+  subject: { type: String, required: true },
+  slot: { type: String, required: true },
+  year: { type: String, required: true },
+  exam: { type: String, enum: ["cat1", "cat2", "fat"], required: true },
 });
 
-const Paper:Model<IPaper> = mongoose.models.Paper || mongoose.model('Paper', PaperSchema);
+const Paper: Model<IPaper> =
+  mongoose.models.Paper ?? mongoose.model<IPaper>("Paper", paperSchema);
 
 export default Paper;
