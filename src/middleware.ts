63cb2aa49306c6d009e2c28f5acecaacc4c4dfad
NishantGupta2325 <nishant.gupta@talentrecruit.com>
2024-06-16 19:44:52 +0530

    middlware fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export function middleware(request: Request) {
   const authtoken = request.headers.get("Authorization");
   console.log("Auth token:", authtoken);
-  if (typeof authtoken === "string" && authtoken.startsWith("Bearer")) {
+  if (typeof authtoken !== "string" ?? authtoken?.startsWith("Bearer")) {
     return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
   }
 }
