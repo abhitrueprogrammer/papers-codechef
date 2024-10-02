bea7cef28fe79ea9daa14c7a627bcc661e9a6387
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-10-02 13:00:33 +0530

    fixed upload route

---
+++ src/app/util/error.tsx
@@ -0,0 +1,18 @@
+import axios, { type AxiosError } from "axios";
+import { ApiError } from "next/dist/server/api-utils";
+import { toSentenceCase } from "./utils";
+
+export function handleAPIError(err: unknown): ApiError {
+  if (axios.isAxiosError(err)) {
+    const error = err as AxiosError;
+    const response = error.response;
+    const data = response?.data;
+    if (data) {
+      const msg = (data as { error: string })?.error;
+      if (msg) {
+        return new ApiError(response.status, toSentenceCase(msg));
+      }
+    }
+  }
+  return new ApiError(500, "Something went wrong");
+}
