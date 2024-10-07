09891d85a7276f55be63c1cb27c3690488c579dd
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-10-07 18:44:02 +0530

    Basic UI fixes and meta add

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,13 +8,15 @@ import Link from "next/link";
 function Navbar() {
   return (
     <div className="flex items-center justify-between px-4 py-6 md:px-12">
-      <div className="hidden w-[20%] md:block">
-        <Image
-          src={ccLogo as HTMLImageElement}
-          alt="codechef-logo"
-          height={70}
-          width={70}
-        />
+      <div className="hiddaen w-[20%] md:block">
+        <a href="https://www.codechefvit.com/">
+          <Image
+            src={ccLogo as HTMLImageElement}
+            alt="codechef-logo"
+            height={70}
+            width={70}
+          />
+        </a>
       </div>
       <Link href="/" className="jost bg-gradient-to-r from-[#562EE7] to-[#FFC6E8] bg-clip-text text-center text-5xl font-bold text-transparent md:w-[60%] md:text-6xl">
         Papers
