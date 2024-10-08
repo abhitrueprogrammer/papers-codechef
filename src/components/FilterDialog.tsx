0c470d385cb50215bff14663316bcdcbc4779808
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-10-08 09:03:13 +0530

    Added close on set filter

--- src/components/FilterDialog.tsx
+++ src/components/FilterDialog.tsx
@@ -60,7 +60,6 @@ export const FilterDialog = ({
 
   const handleFilterClick = () => {
     onApplyFilters(selectedExams, selectedSlots, selectedYears);
-    setOpen(!open);
   };
 
   const handleResetClick = () => {
