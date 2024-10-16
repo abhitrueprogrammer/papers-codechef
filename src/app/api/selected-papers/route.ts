c3ae01f369501df1ab363badd51cb089b58b1c36
NishantGupt786 <nishantgupta2325@gmail.com>
2024-10-16 15:46:09 +0530

    dynamic render

--- src/app/api/selected-papers/route.ts
+++ src/app/api/selected-papers/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { connectToDatabase } from "@/lib/mongoose";
 import Paper from "@/db/papers";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   try {
     await connectToDatabase();
