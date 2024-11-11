39c7081f10770835054303961ba97a8cd978f540
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-11-12 02:11:19 +0530

    added ssr and metadata

--- src/app/paper/[id]/page.tsx
+++ src/app/paper/[id]/page.tsx
@@ -3,10 +3,30 @@ import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import PdfViewer from "@/components/pdfViewer";
 import Loader from "@/components/ui/loader";
+import { PaperResponse } from "@/interface";
+import { Metadata } from "next";
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  const paper: PaperResponse | null = await fetchPaperID(params.id);
 
+  if (paper) {
+    const subject = paper.subject;
+    return {
+      title: `Papers | ${subject}`,
+      openGraph: {
+        title: `Papers | ${subject}`,
+      },
+      twitter: {
+        title: `Papers | ${subject}`,
+      },
+    };
+  }
+
+  return {
+    title: "Paper not found",
+  };
+}
 const PaperPage = async ({ params }: { params: { id: string } }) => {
   const paper = await fetchPaperID(params.id);
-  console.log("HIII");
 
   if (!paper) {
     return <Loader prop="h-screen w-screen" />;
@@ -14,24 +34,14 @@ const PaperPage = async ({ params }: { params: { id: string } }) => {
   return (
     <div>
       <Navbar />
-        <h1 className="jost mb-4 text-center text-2xl font-semibold md:mb-10 md:text-3xl">
-          {paper.subject} {paper.exam} {paper.slot} {paper.year}
-        </h1>
-        <center>
-          <PdfViewer url={paper.finalUrl}></PdfViewer>
-        </center>
+      <h1 className="jost mb-4 text-center text-2xl font-semibold md:mb-10 md:text-3xl">
+        {paper.subject} {paper.exam} {paper.slot} {paper.year}
+      </h1>
+      <center>
+        <PdfViewer url={paper.finalUrl}></PdfViewer>
+      </center>
       <Footer />
     </div>
   );
 };
 export default PaperPage;
-{/* <div>
-<Navbar />
-<div className="flex flex-col items-center justify-center">
-  <h1 className="jost mb-4 text-center text-2xl font-semibold md:mb-10 md:text-3xl">
-    {paper.subject} {paper.exam} {paper.slot} {paper.year}
-  </h1>
-  <PdfViewer url={paper.finalUrl}></PdfViewer>
-</div>
-<Footer />
-</div> */}
