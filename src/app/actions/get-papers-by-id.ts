266df83b41cc05fe28f16238ba9b511ec4090ca1
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-11-13 23:56:59 +0530

    rasied upload rate and added env vars for SSR

--- src/app/actions/get-papers-by-id.ts
+++ src/app/actions/get-papers-by-id.ts
@@ -6,8 +6,12 @@ export const fetchPaperID = async (
   id: string,
 ) => {
   try {
+    if(!process.env.SERVER_URL)
+    {
+      throw "error env not set (server url)"
+    }
     const response: AxiosResponse<PaperResponse> = await axios.get(
-      `http://localhost:3000/api/paper-by-id/${id}`,
+      `${process.env.SERVER_URL}/api/paper-by-id/${id}`,
     );
     return response.data;
   } catch (err: unknown) {
