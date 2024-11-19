4ce2f33d6c1d4c63577081bae9f3cc97f42bb5ae
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-11-19 14:24:54 +0530

    fixed infinite loading on searchbar

--- src/components/searchbarSubjectList.tsx
+++ src/components/searchbarSubjectList.tsx
@@ -32,6 +32,7 @@ function SearchbarSubjectList({
         if (filteredSubjects.length === 0) {
           setError("Subject not found");
           setSuggestions([]);
+          setLoading(false);
           return;
         }
         setSuggestions(filteredSubjects);
