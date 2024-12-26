7ed7fe2b4f339c0fdf8f375ca2d0696236239dcb
abhitrueprogrammer <abhiaruvs3@gmail.com>
2024-12-26 22:01:40 +0530

    add new tags on ongoing papers

--- ongoing-papers.ts
+++ ongoing-papers.ts
@@ -1,51 +1,59 @@
 import { type IPaper } from "@/interface";
 
 const papers: IPaper[] = [
-  // {
-  //   _id: "6708fd8002a75017a4f08759",
-  //   finalUrl:
-  //     "https://res.cloudinary.com/dtorpaj1c/image/upload/v1728642405/papers/orbppimhujcijj2ywhvm.pdf",
-  //   thumbnailUrl:
-  //     "https://res.cloudinary.com/dtorpaj1c/image/upload/w_400,h_400,c_fill/v1728642405/papers/orbppimhujcijj2ywhvm.jpg",
-  //   subject: "Discrete Mathematics and Graph Theory [BMAT205L]",
-  //   slot: "A2",
-  //   year: "2023",
-  //   exam: "CAT-2",
-  // },
-  // {
-  //   _id: "670980523ec3fdad83b2d211",
-  //   finalUrl:
-  //     "https://res.cloudinary.com/dtorpaj1c/image/upload/v1728675912/papers/lvtypsfdtref5mjskley.pdf",
-  //   thumbnailUrl:
-  //     "https://res.cloudinary.com/dtorpaj1c/image/upload/w_400,h_400,c_fill/v1728675912/papers/lvtypsfdtref5mjskley.jpg",
-  //   subject: "Compiler Design [BCSE307L]",
-  //   slot: "A2",
-  //   year: "2024",
-  //   exam: "CAT-2",
-  // },
-  // {
-  //   _id: "670a105e6272bcf9da4e2362",
+  {
+    _id: "6708fd8002a75017a4f08759",
+    finalUrl:
+      "https://res.cloudinary.com/dtorpaj1c/image/upload/v1728642405/papers/orbppimhujcijj2ywhvm.pdf",
+    thumbnailUrl:
+      "https://res.cloudinary.com/dtorpaj1c/image/upload/w_400,h_400,c_fill/v1728642405/papers/orbppimhujcijj2ywhvm.jpg",
+    subject: "Discrete Mathematics and Graph Theory [BMAT205L]",
+    slot: "A2",
+    year: "2023",
+    exam: "CAT-2",
+    semester: "Fall",
+    campus: "Vellore",
+  },
+  {
+    _id: "670980523ec3fdad83b2d211",
+    finalUrl:
+      "https://res.cloudinary.com/dtorpaj1c/image/upload/v1728675912/papers/lvtypsfdtref5mjskley.pdf",
+    thumbnailUrl:
+      "https://res.cloudinary.com/dtorpaj1c/image/upload/w_400,h_400,c_fill/v1728675912/papers/lvtypsfdtref5mjskley.jpg",
+    subject: "Compiler Design [BCSE307L]",
+    slot: "A2",
+    year: "2024",
+    exam: "CAT-2",
+    semester: "Fall",
+    campus: "Vellore",
+  },
+  {
+    _id: "670a105e6272bcf9da4e2362",
 
-  //   finalUrl:
-  //     "https://res.cloudinary.com/dtorpaj1c/image/upload/v1728712773/papers/ifkrnjgwdudtev9rxpki.pdf",
-  //   thumbnailUrl:
-  //     "https://res.cloudinary.com/dtorpaj1c/image/upload/w_400,h_400,c_fill/v1728712773/papers/ifkrnjgwdudtev9rxpki.jpg",
-  //   subject: "Complex Variables and Linear Algebra [BMAT201L]",
-  //   slot: "A1",
-  //   year: "2023",
-  //   exam: "CAT-2",
-  // },
-  // {
-  //   _id: "67097e7b3ec3fdad83b2d205",
-  //   finalUrl:
-  //     "https://res.cloudinary.com/dtorpaj1c/image/upload/v1728675439/papers/pyyqotz1mzuh2wmq1k9s.pdf",
-  //   thumbnailUrl:
-  //     "https://res.cloudinary.com/dtorpaj1c/image/upload/w_400,h_400,c_fill/v1728675439/papers/pyyqotz1mzuh2wmq1k9s.jpg",
-  //   subject: "Computer Networks [BCSE308L]",
-  //   slot: "C2",
-  //   year: "2023",
-  //   exam: "CAT-2",
-  // },
+    finalUrl:
+      "https://res.cloudinary.com/dtorpaj1c/image/upload/v1728712773/papers/ifkrnjgwdudtev9rxpki.pdf",
+    thumbnailUrl:
+      "https://res.cloudinary.com/dtorpaj1c/image/upload/w_400,h_400,c_fill/v1728712773/papers/ifkrnjgwdudtev9rxpki.jpg",
+    subject: "Complex Variables and Linear Algebra [BMAT201L]",
+    slot: "A1",
+    year: "2023",
+    exam: "CAT-2",
+    semester: "Fall",
+    campus: "Vellore",
+  },
+  {
+    _id: "67097e7b3ec3fdad83b2d205",
+    finalUrl:
+      "https://res.cloudinary.com/dtorpaj1c/image/upload/v1728675439/papers/pyyqotz1mzuh2wmq1k9s.pdf",
+    thumbnailUrl:
+      "https://res.cloudinary.com/dtorpaj1c/image/upload/w_400,h_400,c_fill/v1728675439/papers/pyyqotz1mzuh2wmq1k9s.jpg",
+    subject: "Computer Networks [BCSE308L]",
+    slot: "C2",
+    year: "2023",
+    exam: "CAT-2",
+    semester: "Fall",
+    campus: "Vellore",
+  },
 ];
 
 export default papers;
